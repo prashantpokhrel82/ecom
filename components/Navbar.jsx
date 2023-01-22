8ce3af1ec3991b5ac613113ef624ac43e4ccfe9d
Prashant <prashantpokhrel15@gmail.com>
2023-01-22 22:28:34 +1100

    cart component completed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
+import { useStateContext } from "../context/StateContext";
+import Cart from "./Cart";
 
 const Navbar = () => {
+  const { totalQuantities, showCart, toggleCart } = useStateContext();
   return (
     <div className="navbar-container">
       <p className="logo">
         <Link href="/">Marty Shopping</Link>
       </p>
-      <button type="button" className="cart-icon" onClick="">
+      <button type="button" className="cart-icon" onClick={toggleCart}>
         <AiOutlineShopping />
-        <span className="cart-item-qty">1</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
+      {showCart && <Cart />}
     </div>
   );
 };
